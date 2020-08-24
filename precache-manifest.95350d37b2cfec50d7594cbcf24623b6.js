3f06c62acebb5dab27072e132bd55dafc7bb1e5c
teobot <theoclapperton@outlook.com>
2020-08-24 14:59:40 +0100

    Updates

---
+++ precache-manifest.95350d37b2cfec50d7594cbcf24623b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13454d8f5a46c69ddca1d49c3d779d6c",
+    "url": "/portfolio-build/index.html"
+  },
+  {
+    "revision": "26135a537a3e4f85ef2a",
+    "url": "/portfolio-build/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1f3c3b7f24319a99ef6e",
+    "url": "/portfolio-build/static/css/main.ed581ff2.chunk.css"
+  },
+  {
+    "revision": "26135a537a3e4f85ef2a",
+    "url": "/portfolio-build/static/js/2.de788a92.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-build/static/js/2.de788a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3c3b7f24319a99ef6e",
+    "url": "/portfolio-build/static/js/main.d2f7e6ea.chunk.js"
+  },
+  {
+    "revision": "9684bab4a19b3c8d5345",
+    "url": "/portfolio-build/static/js/runtime-main.93e55659.js"
+  },
+  {
+    "revision": "7ab93d000328a4b3252ce49a15414d85",
+    "url": "/portfolio-build/static/media/sig-invert.7ab93d00.png"
+  },
+  {
+    "revision": "63b2568eb5ee51734dee0611ac124757",
+    "url": "/portfolio-build/static/media/sig.63b2568e.png"
+  }
+]);
